0561ab24b6fa9f74cef13a62cfce8e016e9c8cd7
nomin02 <flvlclq89@gmail.com>
2023-07-26 11:57:22 +0800

    26th July

---
+++ 2.py
@@ -0,0 +1,21 @@
+# --------------------------------------------
+# aravtiin toolol = %d
+# burtarhai too = %f
+# print("time is %d:%d" %(14, 28))
+
+# 소수는 다르게 표현이 된다.
+# 3.1이면 butarhai toonii tsegees urdah 3 oron,
+# tsegiin ardah 1 oron ashiglagdana.
+
+# 숫자들을 대체하는 포맷이 있다.
+# 예시 :
+num1 =int(input("num1: "))
+num2 =int(input("num2: "))
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+print("%d + %d = %d" %(num1, num2, result1))
+print("%d - %d = %d" %(num1, num2, result2))
+print("%d * %d = %d" %(num1, num2, result3))
+print("%d / %d = %d" %(num1, num2, result4))
---
+++ 3.py
@@ -0,0 +1,18 @@
+#list
+##alist = [10, 1, 3, 42, 100]
+##print(alist[0])
+##print(alist[1])
+##print(alist[2])
+#2번 인덱스 바로 뒤에 있는 거 까지 프린트 가능하다.
+##a = [1,2,[3,4,5], 'python']
+##print(a[:2])
+##print(a[0:3])
+##print(a[2:]) # 2-r indexees hoish bugdiig hevleh
+##print(a[2][1:])# 2-r indexiin 1-r indexees hoish bugdiig hevleh
+##print(a[3][:3])# 2-r indexiin 3 hurtel
+##
+#키 값과 밸류 값이 있다. 키는 인덱스와 비슷하다.
+a = {"type" : "C++", "num":2, 42:3}
+print(a["type"])
+inputs= [input() for _ in range(5)]
+print(inputs)
