cf4ad2b70026baeea0864a2de64bb4df43dfd9eb
nomin02 <flvlclq89@gmail.com>
2023-07-26 16:17:47 +0800

    if_test

---
+++ if_test.py
@@ -0,0 +1,11 @@
+import sys
+n = int(input("too bich : "))
+
+if(n < 0):
+    print("surug too bna.")
+else:
+    if(n % 2 == 0):
+        print("tegsh too")
+    else:
+        print("sondgoi too")
+
